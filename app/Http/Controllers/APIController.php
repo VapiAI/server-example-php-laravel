e5a75d4352d29b57d4cefcd73ebebf1c4880665e
kaustubh <kaustubhkagrawal@gmail.com>
2024-02-07 17:23:01 +0530

    Basic endpoints placeholder

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -8,40 +8,41 @@ class APIController extends Controller
 {
     public function inbound(Request $request)
     {
-        //
+        return 'inbound';
     }
+
     public function outbound(Request $request)
     {
-        //
+        return 'outbound';
     }
 
     public function functionsBasic(Request $request)
     {
-        //
+        return 'functions/basic';
     }
 
     public function functionsRag(Request $request)
     {
-        //
+        return 'functions/rag';
     }
 
     public function customLlmBasic(Request $request)
     {
-        //
+        return 'custom-llm/basic';
     }
 
     public function customLlmOpenaiSse(Request $request)
     {
-        //
+        return 'custom-llm/openai-sse';
     }
 
     public function customLlmOpenaiAdvanced(Request $request)
     {
-        //
+        return 'custom-llm/openai-advanced';
     }
 
     public function webhook(Request $request)
     {
-        //
+        return 'webhook';
     }
 }
