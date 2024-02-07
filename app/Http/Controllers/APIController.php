faeeeb40023ba60d37b31a7f3eafb4b5666f3436
kaustubh <kaustubhkagrawal@gmail.com>
2024-02-07 16:46:49 +0530

    API setup

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
-    //
+    public function inbound(Request $request)
+    {
+        //
+    }
+    public function outbound(Request $request)
+    {
+        //
+    }
+
+    public function functionsBasic(Request $request)
+    {
+        //
+    }
+
+    public function functionsRag(Request $request)
+    {
+        //
+    }
+
+    public function customLlmBasic(Request $request)
+    {
+        //
+    }
+
+    public function customLlmOpenaiSse(Request $request)
+    {
+        //
+    }
+
+    public function customLlmOpenaiAdvanced(Request $request)
+    {
+        //
+    }
+
+    public function webhook(Request $request)
+    {
+        //
+    }
 }
